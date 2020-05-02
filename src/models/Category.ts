88ce42b9cca84fc3a56091affc4d8ea867639506
Vinicius <viniciusflores379@gmail.com>
2020-05-02 14:16:56 -0300

    :art: refactor to desafio reactjs

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -4,7 +4,9 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm'
+import Transaction from './Transaction'
 
 @Entity('categories')
 class Category {
@@ -13,6 +15,9 @@ class Category {
 
   @Column('varchar')
   title: string
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction
 
   @CreateDateColumn()
   created_at: Date
